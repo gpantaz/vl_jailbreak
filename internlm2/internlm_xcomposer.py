d4ff4dbdb3684f4ba736fb1cb65d59d7b7b8b087
George Pantazopoulos <georgepantaz94@gmail.com>
2024-02-26 21:39:13 +0000

    feat: internlm_xcomposer code

---
+++ internlm2/internlm_xcomposer.py
@@ -0,0 +1,113 @@
+import argparse
+import os
+import json
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers.generation import GenerationConfig
+import torch
+
+import sys
+from tqdm import tqdm
+
+sys.path.append("./")
+from dataset_iterator import DatasetIterator
+
+
+def main(args):
+    dataset = DatasetIterator(
+        question_csv_path=args.question_csv_path,
+        jailbreak_csv=args.jailbreak_csv,
+        images_folder_path=args.images_folder_path,
+        use_jailbreak_prompt=args.use_jailbreak_prompt,
+        use_blank_image=args.use_blank_image,
+    )
+
+    # Note: The default behavior now has injection attack prevention off.
+    tokenizer = AutoTokenizer.from_pretrained(
+        "internlm/internlm-xcomposer2-7b", trust_remote_code=True
+    )
+
+    model = AutoModelForCausalLM.from_pretrained(
+        "internlm/internlm-xcomposer2-7b", device_map="cuda", trust_remote_code=True
+    ).eval()
+    generation_config = GenerationConfig(
+        max_length=1024,
+        top_k=1,
+        top_p=1,
+        temperature=1,
+        num_return_sequences=1,
+        do_sample=False,
+        eos_token_id=151643,
+        max_new_tokens=1024,
+        max_window_size=6144,
+        pad_token_id=151643,
+        repetition_penalty=1.0,
+        chat_format="chatml",
+    )
+
+    output_json = args.output_json
+    for example in tqdm(dataset, total=len(dataset), desc="Running model"):
+        # response, history = model.chat(tokenizer, example.prompt, history=None, generation_config=generation_config)
+
+        images = [model.vis_processor(example.image)]
+        image = torch.stack(images).to(model.device)
+
+        query = f"<ImageHere> <ImageHere>{example.prompt}"
+        response, history = model.chat(
+            tokenizer, query=query, history=None, generation_config=generation_config
+        )
+        breakpoint()
+
+        # query = tokenizer.from_list_format(
+        #     [{'image': "/users/gmp2000/sharedscratch/vl_jailbreak/data/images/harmful_content_0.jpg"},{'text': "a photo of a"},
+        #     ])
+
+        if os.path.exists(output_json):
+            data = json.load(open(output_json))
+        else:
+            data = {}
+
+        data[example.to_key()] = response
+        with open(output_json, "w") as f:
+            json.dump(data, f, indent=4)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--question-csv-path",
+        type=str,
+        default="data/prompt_selection.csv",
+        help="Path to the question csv file.",
+    )
+    parser.add_argument(
+        "--jailbreak-csv",
+        type=str,
+        default="data/jailbreak-prompts.csv",
+        help="Path to the jailbreak csv file.",
+    )
+    parser.add_argument(
+        "--images-folder-path",
+        type=str,
+        default="data/images",
+        help="Path to the images folder.",
+    )
+    parser.add_argument(
+        "--use-jailbreak-prompt",
+        action="store_true",
+        help="Whether use jailbreak prompt.",
+    )
+    parser.add_argument(
+        "--use-blank-image",
+        action="store_true",
+        help="Whether use blank image.",
+    )
+
+    parser.add_argument(
+        "--output-json",
+        type=str,
+        default="predictions/interlm_xcomposer.json",
+        help="Path to the output json file.",
+    )
+    args = parser.parse_args()
+
+    main(args)
