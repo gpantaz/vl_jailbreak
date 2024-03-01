7ca34948f91e5e25825ce8ddfdf0f860b63780b8
George Pantazopoulos <georgepantaz94@gmail.com>
2024-03-01 14:34:47 +0000

    feat: minor

--- scripts/run_llava_eval_sampling.sh
+++ scripts/run_llava_eval_sampling.sh
@@ -1,8 +1,8 @@
-python vicua/vicuna --output-json predictions_sampling/vicuna_jailbreakFalse_1.json --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
-python vicua/vicuna --output-json predictions_sampling/vicuna_jailbreakFalse_2.json --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
-python vicua/vicuna --output-json predictions_sampling/vicuna_jailbreakFalse_3.json --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
-python vicua/vicuna --output-json predictions_sampling/vicuna_jailbreakFalse_4.json --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
-python vicua/vicuna --output-json predictions_sampling/vicuna_jailbreakFalse_5.json --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
+python vicua/vicuna.py --output-json predictions_sampling/vicuna_jailbreakFalse_1.json --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
+python vicua/vicuna.py --output-json predictions_sampling/vicuna_jailbreakFalse_2.json --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
+python vicua/vicuna.py --output-json predictions_sampling/vicuna_jailbreakFalse_3.json --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
+python vicua/vicuna.py --output-json predictions_sampling/vicuna_jailbreakFalse_4.json --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
+python vicua/vicuna.py --output-json predictions_sampling/vicuna_jailbreakFalse_5.json --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
 
 python llava/llava_eval.py --output-json predictions_sampling/llava_jailbreakFalse_1.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
 python llava/llava_eval.py --output-json predictions_sampling/llava_jailbreakFalse_2.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
@@ -16,11 +16,11 @@ python llava/llava_eval.py --use-blank-image --output-json predictions_sampling/
 python llava/llava_eval.py --use-blank-image --output-json predictions_sampling/blank_image_llava_jailbreakFalse_4.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
 python llava/llava_eval.py --use-blank-image --output-json predictions_sampling/blank_image_llava_jailbreakFalse_5.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
 
-python vicua/vicuna --use-jailbreak-prompt --output-json predictions_sampling/vicuna_jailbreakTrue_1.json --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
-python vicua/vicuna --use-jailbreak-prompt --output-json predictions_sampling/vicuna_jailbreakTrue_2.json --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
-python vicua/vicuna --use-jailbreak-prompt --output-json predictions_sampling/vicuna_jailbreakTrue_3.json --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
-python vicua/vicuna --use-jailbreak-prompt --output-json predictions_sampling/vicuna_jailbreakTrue_4.json --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
-python vicua/vicuna --use-jailbreak-prompt --output-json predictions_sampling/vicuna_jailbreakTrue_5.json --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
+python vicua/vicuna.py --use-jailbreak-prompt --output-json predictions_sampling/vicuna_jailbreakTrue_1.json --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
+python vicua/vicuna.py --use-jailbreak-prompt --output-json predictions_sampling/vicuna_jailbreakTrue_2.json --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
+python vicua/vicuna.py --use-jailbreak-prompt --output-json predictions_sampling/vicuna_jailbreakTrue_3.json --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
+python vicua/vicuna.py --use-jailbreak-prompt --output-json predictions_sampling/vicuna_jailbreakTrue_4.json --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
+python vicua/vicuna.py --use-jailbreak-prompt --output-json predictions_sampling/vicuna_jailbreakTrue_5.json --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
 
 python llava/llava_eval.py --use-jailbreak-prompt --output-json predictions_sampling/llava_jailbreakTrue_1.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
 python llava/llava_eval.py --use-jailbreak-prompt --output-json predictions_sampling/llava_jailbreakTrue_2.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
