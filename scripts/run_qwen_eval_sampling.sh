410189d1a0d485c3f1616f4c0a44d3dcbc1d4088
George Pantazopoulos <georgepantaz94@gmail.com>
2024-03-01 09:52:07 +0000

    feat: minor

--- scripts/run_qwen_eval_sampling.sh
+++ scripts/run_qwen_eval_sampling.sh
@@ -10,11 +10,11 @@ python qwen/qwen_vl_evaluate.py --output-json predictions_sampling/qwen_vl_jailb
 python qwen/qwen_vl_evaluate.py --output-json predictions_sampling/qwen_vl_jailbreakFalse_4.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
 python qwen/qwen_vl_evaluate.py --output-json predictions_sampling/qwen_vl_jailbreakFalse_5.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
 
-python qwen/qwen_vl_evaluate.py --use-blank-image --output-json predictions_sampling/qwen_vl_jailbreakFalse_1.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
-python qwen/qwen_vl_evaluate.py --use-blank-image --output-json predictions_sampling/qwen_vl_jailbreakFalse_2.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
-python qwen/qwen_vl_evaluate.py --use-blank-image --output-json predictions_sampling/qwen_vl_jailbreakFalse_3.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
-python qwen/qwen_vl_evaluate.py --use-blank-image --output-json predictions_sampling/qwen_vl_jailbreakFalse_4.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
-python qwen/qwen_vl_evaluate.py --use-blank-image --output-json predictions_sampling/qwen_vl_jailbreakFalse_5.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
+python qwen/qwen_vl_evaluate.py --use-blank-image --output-json predictions_sampling/blank_image_qwen_vl_jailbreakFalse_1.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
+python qwen/qwen_vl_evaluate.py --use-blank-image --output-json predictions_sampling/blank_image_qwen_vl_jailbreakFalse_2.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
+python qwen/qwen_vl_evaluate.py --use-blank-image --output-json predictions_sampling/blank_image_qwen_vl_jailbreakFalse_3.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
+python qwen/qwen_vl_evaluate.py --use-blank-image --output-json predictions_sampling/blank_image_qwen_vl_jailbreakFalse_4.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
+python qwen/qwen_vl_evaluate.py --use-blank-image --output-json predictions_sampling/blank_image_qwen_vl_jailbreakFalse_5.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
 
 python qwen/qwen_evaluate.py --use-jailbreak-prompt --output-json predictions_sampling/qwen_jailbreakTrue_1.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
 python qwen/qwen_evaluate.py --use-jailbreak-prompt --output-json predictions_sampling/qwen_jailbreakTrue_2.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
@@ -28,8 +28,8 @@ python qwen/qwen_vl_evaluate.py --use-jailbreak-prompt --output-json predictions
 python qwen/qwen_vl_evaluate.py --use-jailbreak-prompt --output-json predictions_sampling/qwen_vl_jailbreakTrue_4.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
 python qwen/qwen_vl_evaluate.py --use-jailbreak-prompt --output-json predictions_sampling/qwen_vl_jailbreakTrue_5.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
 
-python qwen/qwen_vl_evaluate.py --use-jailbreak-prompt --use-blank-image --output-json predictions_sampling/qwen_vl_jailbreakTrue_1.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
-python qwen/qwen_vl_evaluate.py --use-jailbreak-prompt --use-blank-image --output-json predictions_sampling/qwen_vl_jailbreakTrue_2.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
-python qwen/qwen_vl_evaluate.py --use-jailbreak-prompt --use-blank-image --output-json predictions_sampling/qwen_vl_jailbreakTrue_3.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
-python qwen/qwen_vl_evaluate.py --use-jailbreak-prompt --use-blank-image --output-json predictions_sampling/qwen_vl_jailbreakTrue_4.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
-python qwen/qwen_vl_evaluate.py --use-jailbreak-prompt --use-blank-image --output-json predictions_sampling/qwen_vl_jailbreakTrue_5.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
+python qwen/qwen_vl_evaluate.py --use-jailbreak-prompt --use-blank-image --output-json predictions_sampling/blank_image_qwen_vl_jailbreakTrue_1.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
+python qwen/qwen_vl_evaluate.py --use-jailbreak-prompt --use-blank-image --output-json predictions_sampling/blank_image_qwen_vl_jailbreakTrue_2.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
+python qwen/qwen_vl_evaluate.py --use-jailbreak-prompt --use-blank-image --output-json predictions_sampling/blank_image_qwen_vl_jailbreakTrue_3.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
+python qwen/qwen_vl_evaluate.py --use-jailbreak-prompt --use-blank-image --output-json predictions_sampling/blank_image_qwen_vl_jailbreakTrue_4.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
+python qwen/qwen_vl_evaluate.py --use-jailbreak-prompt --use-blank-image --output-json predictions_sampling/blank_image_qwen_vl_jailbreakTrue_5.json --do-sample --top-p 0.9 --jailbreak-csv data/jailbreak-prompts2.csv
